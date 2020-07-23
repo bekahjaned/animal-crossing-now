2f91a4ebcac6d70f29587d817681c1816618b13f
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-07-22 21:42:14 -0400

    Moved the logic of getting an array of fish depending on a certain criteria into a separate function. The function will return the array, which will be called with the user's month and time. Then a new variable is created to loop through each fish in the array and display it in the component. The variable is in the JSX part of the code, so that the CritterCard component will go there on the web app.

--- src/Components/FishDisplay.js
+++ src/Components/FishDisplay.js
@@ -45,22 +45,36 @@ class FishDisplay extends React.Component {
     }));
   };
 
-  getFishAvailability = (userMonth, userTime) => {
+  // This is a function to filter though the fish file and
+  // only give back a list of the fish available right now, in this hemisphere
+  fishAvailableNow = (userMonth, userTime) => {
+    // using filter in order to create a new array of fish that match a certain criteria
     const availableFish = this.state.fishes.filter((fish) => {
+      // destructuring object properties of each fish to make code short and simple
       const { hemisphere, hours } = fish.availability;
       return (
+        // Searching for and chosing the hemisphere of the fish to match user's hemisphere from state
+        // also checking if the array of months in the fish's availability (for the specific hemisphere) includes the user's month
         hemisphere[this.state.user.hemisphere].includes(userMonth) &&
+        // checking if the array of hours in the fish's availability includes the user's time
         hours.includes(userTime)
+        // if the above statements are true, add it to the array 'availableFish'
       );
     });
+    // returning the final array of available fish and waiting to be called
     return availableFish;
   };
 
   render() {
+    // destructuring object properties of user to make code short and simple
     const { month, time } = this.state.user;
-    const allAvailableFish = this.getFishAvailability(month, time);
-    const availableFish = allAvailableFish.map((fish) => {
+    // creating a variable to hold the outcome of 'fishAvailableNow', AKA 'availableFish' from line 64
+    // using user's time and month from state to call this function
+    const fishAvailableNow = this.fishAvailableNow(month, time);
+    // creating a variable to be placed in return code below that loops though every available fish
+    const availableFish = fishAvailableNow.map((fish) => {
       return (
+        // displaying certain properties in the component
         <CritterCard
           image={fish.image}
           name={fish.name}
@@ -84,6 +98,7 @@ class FishDisplay extends React.Component {
             </select>
           </label>
         </div>
+        {/* placing the variable here to show the CritterCard component */}
         <div className="critter-grid">{availableFish}</div>
       </div>
     );
