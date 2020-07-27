969446dd377e77e31714617619ec0dfddd305094
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-07-27 15:51:33 -0400

    Add styling to Footer.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 class Footer extends React.Component {
   render() {
     return (
-      <div>
+      <div className="footer">
         If you find this site helpful, you can buy us a coffee at (link).
       </div>
     );
