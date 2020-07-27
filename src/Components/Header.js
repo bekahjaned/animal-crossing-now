ab085106196ab123038f827a36a1fb8d4872b655
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-07-27 15:09:59 -0400

    Add descriptive paragraph to Header.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,11 @@ class Header extends React.Component {
   render() {
     return (
       <div className="center">
-        <h1>Animal Crossing Now!</h1>
+        <h1>Animal Crossing Now</h1>
+        <p>
+          List of fish and bugs available right now for Animal Crossing: New
+          Horizons
+        </p>
         <Time />
       </div>
     );
