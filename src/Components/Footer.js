3d888f7996508947764db5c9a94e7332f495e679
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-07-27 14:55:11 -0400

    Create a footer component.

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+        If you find this helpful and want to support us, send us a coffee here
+        (insert link)
+      </div>
+    );
+  }
+}
+
+export default Footer;
