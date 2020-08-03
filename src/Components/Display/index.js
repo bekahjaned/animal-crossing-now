dba64f44d1da94194a973a9d061c269774c6bcde
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-08-03 13:53:17 -0400

    Replace Footer with a styled component and place in App instead of Display

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import Time from "../Time/";
 import FishCard from "../FishCard/";
 import BugCard from "../BugCard/";
-import Footer from "../Footer/";
 
 import fishes from "../../data/fishes.json";
 import bugs from "../../data/bugs.json";
@@ -114,7 +113,6 @@ class Display extends React.Component {
         <div className="critter-grid">{availableFish}</div>
         <h3 className="critter-display">Bugs available now</h3>
         <div className="critter-grid">{availableBugs}</div>
-        <Footer />
       </div>
     );
   }
--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -3,12 +3,16 @@ import "../App/index.css";
 
 import Header from "../../Components/Header";
 import Display from "../../Components/Display";
+import { Footer } from "../../Elements/Footer";
 
 function App() {
   return (
     <div className="content">
       <Header />
       <Display />
+      <Footer>
+        If you find this site helpful, you can buy us a coffee at (link).
+      </Footer>
     </div>
   );
 }
