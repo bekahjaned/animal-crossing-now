73229c197aff9112beb9f95c5a5dee6ce6e04e89
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-08-03 14:01:21 -0400

    Replace Header with styled component

---
+++ src/Elements/Header.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  grid-column: 1 / 13;
+  text-align: center;
+  font-size: 18px;
+  margin-bottom: 40px;
+  h1 {
+    font-size: 45px;
+    font-weight: 700;
+    margin-bottom: 8px;
+  }
+  p {
+    margin: 0;
+  }
+`;
