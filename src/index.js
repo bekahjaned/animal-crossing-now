e40a78b3e761bd87c17d18667be5c2e8cb04bd63
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-08-03 12:00:32 -0400

    Relink App to main index.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "../src/Pages/App/";
+import App from "../src/Pages/App/index.js";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
